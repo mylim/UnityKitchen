eb3920c466f155eb7b9264919adcb8f4b6a1c688
Meiyii <M.Lim@hw.ac.uk>
2016-06-29 07:32:30 +0100

    17/06/16 Trying out raycasting using finger pointing (not working) and grab

--- Kitchen/Assets/GrabCube.cs
+++ Kitchen/Assets/GrabCube.cs
@@ -12,16 +12,86 @@ public class GrabCube : MonoBehaviour {
     // Use this for initialization
     void Start () {
         controller = new Controller();
-        listener = new SampleListener();
+        
+        /*listener = new SampleListener();
         controller.Connect += listener.OnServiceConnect;
         controller.Device += listener.OnConnect;
-        controller.FrameReady += listener.OnFrame;
+        controller.FrameReady += listener.OnFrame;*/
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    GameObject GetMouseHoverObject(float range)
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit rayCastHit;
+        Vector3 target = position + Camera.main.transform.forward;
+        if (Physics.Linecast(position, target, out rayCastHit))
+            return rayCastHit.collider.gameObject;
+        return null;
+    }
+
+    void Update()
+    {
+        Debug.Log(GetMouseHoverObject(5));
+    }
+
+    /*void UpdatePinch(Frame frame)
+    {
+        bool trigger_pinch = false;
+        Hand hand = frame.Hands[handIndex];
+	    // Thumb tip is the pinch position.
+        Vector3 thumb_tip = hand.Fingers[0].TipPosition.ToUnityScaled();
+     
+        // Check thumb tip distance to joints on all other fingers.
+        // If it's close enough, start pinching.
+
+        for (int i = 1; i < NUM_FINGERS && !trigger_pinch; ++i)
+        {
+            Finger finger = hand.Fingers[i];
+
+            for (int j = 0; j < NUM_JOINTS && !trigger_pinch; ++j)
+            {
+                Vector3 joint_position = finger.JointPosition((Finger.FingerJoint)(j)).ToUnityScaled();
+
+                Vector3 distance = thumb_tip - joint_position;
+
+                if (distance.magnitude < THUMB_TRIGGER_DISTANCE)
+
+                    trigger_pinch = true;
+            }           
+        }
+
+	    // Only change state if it's different.
+
+        if (trigger_pinch && !pinching_)
+          OnPinch(pinch_position);
+    }
+
+    void OnPinch(Vector3 pinch_position)
+    {
+        // ...
+        // Check if we pinched a movable object and grab the closest one.
+        Collider[] close_things = Physics.OverlapSphere(pinch_position, PINCH_DISTANCE, layer_mask_);
+        Vector3 distance = new Vector3(PINCH_DISTANCE, 0.0f, 0.0f);
+        for (int j = 0; j < close_things.Length; ++j)
+        {
+            Vector3 new_distance = pinch_position - close_things[j].transform.position;
+            if (close_things[j].rigidbody != null && new_distance.magnitude < distance.magnitude)
+            {
+                grabbed_object_ = close_things[j];
+                distance = new_distance;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Accelerate what we are grabbing toward the pinch.
+        if (grabbed_ != null)
+        {
+            Vector3 distance = pinch_position - grabbed_.transform.position;
+            grabbed_object_.rigidbody.AddForce(SPRING_CONSTANT * distance);
+        }
+    }*/
 
     void Stop ()
     {
--- Kitchen/Assets/Scripts/InputHandler.cs
+++ Kitchen/Assets/Scripts/InputHandler.cs
@@ -2,18 +2,26 @@
 using System.Collections;
 using Leap;
 using System.Collections.Generic;
+using Leap.Unity;
 
 public class InputHandler : MonoBehaviour {
     Animator animator;
     bool open = false;
-    /*public bool openable;
-    /Controller controller;
+    //public bool openable;
+    //Controller controller;
+
+    //HandModel hand_model;
+    //Hand leap_hand;
 
     // Use this for initialization
     void Start()
     {
-        controller = new Controller();
-    }*/
+        //controller = new Controller();
+
+        /*hand_model = GetComponent<HandModel>();
+        leap_hand = hand_model.GetLeapHand();
+        if (leap_hand == null) Debug.LogError("No leap_hand founded");*/
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,12 +32,13 @@ public class InputHandler : MonoBehaviour {
             RaycastHit rayCastHit;
 
             // Debug ray
-            // Debug.DrawRay(ray.origin, ray.direction, Color.green);
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.green, 2f);
+            Debug.Log("Ray direction " + ray.direction.ToString());
 
             if (Physics.Raycast(ray.origin, ray.direction, out rayCastHit, 1.5f) && rayCastHit.collider.isTrigger)
             {
                 //Debug.Log("Right mouse button click!");
-                Debug.Log(rayCastHit.collider.gameObject.name);
+                //Debug.Log(rayCastHit.collider.gameObject.name);
                 //if (rayCastHit.collider.tag == "FridgeRightDoor")
                 {
                     //FridgeDoorAnim fridgeDoorAnim = rayCastHit.transform.GetComponent<FridgeDoorAnim>();
@@ -60,13 +69,18 @@ public class InputHandler : MonoBehaviour {
                     Finger.FingerType fingerType = finger.Type;
                     if (fingerType.Equals(Finger.FingerType.TYPE_INDEX))
                     {
-                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        Ray ray = Camera.main.ScreenPointToRay(finger.Direction.ToVector3());
+                        Debug.Log("Finger position " + finger.TipPosition.ToVector3().ToString());
+                        Debug.Log("Finger direction" + finger.Direction.ToVector3().ToString());
+                        Debug.Log("Ray direction " + ray.direction.ToString());
+                        Debug.Log("Ray origin " + ray.origin.ToString());
                         RaycastHit rayCastHit;
 
                         // Debug ray
-                        // Debug.DrawRay(ray.origin, ray.direction, Color.green);
+                        Debug.DrawRay(finger.TipPosition.ToVector3(), finger.Direction.ToVector3() * 10f, Color.red, 2f);
+                        //Debug.DrawRay(ray.origin, ray.direction * 20f, Color.green, 2f);
 
-                        if (Physics.Raycast(ray.origin, ray.direction, out rayCastHit, 1.5f))
+                        if (Physics.Raycast(ray.origin, ray.direction, out rayCastHit, 1.5f) && rayCastHit.collider.isTrigger)
                         {
                             //Debug.Log("Right mouse button click!");
                             Debug.Log(rayCastHit.collider.gameObject.name);
@@ -90,6 +104,14 @@ public class InputHandler : MonoBehaviour {
                     }
                 }
             }
+        }*/
+
+        /*for (int i = 0; i < HandModel.NUM_FINGERS; i++)
+        {
+            FingerModel finger = hand_model.fingers[i];
+            Debug.Log("Finger " + finger.name);
+            // draw ray from finger tips (enable Gizmos in Game window to see)
+            Debug.DrawRay(finger.GetTipPosition(), finger.GetRay().direction, Color.red);
         }*/
 
     }
