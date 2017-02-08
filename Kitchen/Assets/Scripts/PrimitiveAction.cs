7d900121a6f09370e8534e0c45ed7216a6cd8600
Meiyii <M.Lim@hw.ac.uk>
2017-02-08 14:22:12 +0000

    08/02/17 Solved CS0066 error in IHandModel

--- Kitchen/Assets/Scripts/PrimitiveAction.cs
+++ Kitchen/Assets/Scripts/PrimitiveAction.cs
@@ -2,18 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Action : MonoBehaviour {
-
+public class PrimitiveAction : MonoBehaviour {
     private List<Errand> errands;
     private int errandIndex;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
